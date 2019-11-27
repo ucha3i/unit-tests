f6150e26c0c0afa632ad000d5f19be5bf9ec0f80
ucha3i <jchabik@gmail.com>
2019-11-27 13:09:33 +0100

    7 tests passed correctly

--- src/5-month-name.js
+++ src/5-month-name.js
@@ -1,8 +1,11 @@
 const months = [
-  'January', 'February', 'March', 'April', 'May', 'June',
-  'July', 'August', 'September', 'October', 'November', 'December'
+    'January', 'February', 'March', 'April', 'May', 'June',
+    'July', 'August', 'September', 'October', 'November', 'December'
 ]
 
 export const monthName = (monthNumber) => {
-  return months[monthNumber]
+    if (monthNumber < 1 || monthNumber > 12) {
+        return null
+    }
+    return months[monthNumber - 1]
 }
